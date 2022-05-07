f697c64a7be5f88ebdf061ef3087e1ed6e3a3d78
davidhudson21 <71290098+davidhudson2128@users.noreply.github.com>
2022-05-07 14:37:20 -0400

    Changing comment
    Changing comment to be more clear

--- sportsipy/ncaab/issue_test.py
+++ sportsipy/ncaab/issue_test.py
@@ -3,7 +3,7 @@ from sportsipy.ncaab.roster import Roster, Player
 # Player who hasn't played in 2021-22 season
 player = Player('quinlan-bennett-1')
 
-print(player('2021-22').points)  # Expected: 0      Actual: 144
-print(player('2041-42').points)  # Expected: 0      Actual: 144
+print(player('2021-22').points)  # Expected: 0      Before Fix: 144
+print(player('2041-42').points)  # Expected: 0      Before Fix: 144
 
 
